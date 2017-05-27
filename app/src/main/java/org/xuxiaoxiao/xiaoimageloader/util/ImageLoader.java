21aa62f04ddf978382a33e6f8208b2074e67ec15
xiaoqianghou <2662651103@qq.com>
2017-05-27 20:37:31 +0800

    ImageLoader变量初始化

--- app/src/main/java/org/xuxiaoxiao/xiaoimageloader/util/ImageLoader.java
+++ app/src/main/java/org/xuxiaoxiao/xiaoimageloader/util/ImageLoader.java
@@ -1,11 +1,54 @@
 package org.xuxiaoxiao.xiaoimageloader.util;
 
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.util.LruCache;
+
+import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+
 /**
  * Created by WuQiang on 2017/5/27.
  */
 
 public class ImageLoader {
     private static ImageLoader mInstance;
+
+    /**
+     * 图片缓存的核心对象
+     */
+    private LruCache<String,Bitmap> mLruCache;
+
+    /**
+     * 线程池
+     */
+    private ExecutorService mThreadPool;
+    // 默认线程数
+    private static final int DEFAULT_THREAD_COUNT = 1;
+
+    /**
+     * 队列的调度方式
+     */
+    private Type mType = Type.LIFO;
+    /**
+     * 任务队列
+     */
+    private LinkedList<Runnable> mTaskQueue;
+
+    /**
+     * 后台轮询线程
+     */
+    private Thread mPoolThread;
+    private Handler mPoolThreadHandler;
+
+    /**
+     * UI线程中的Handler
+     */
+    private Handler mUIHandler;
+    public enum Type
+    {
+        FIFI,LIFO;
+    }
 
     private ImageLoader(){
 

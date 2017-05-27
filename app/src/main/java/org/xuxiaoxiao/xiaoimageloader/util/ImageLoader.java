33f373e62554c63981e35a481cdf812edee57a4e
xiaoqianghou <2662651103@qq.com>
2017-05-27 23:07:19 +0800

    初始化mUIHandler

--- app/src/main/java/org/xuxiaoxiao/xiaoimageloader/util/ImageLoader.java
+++ app/src/main/java/org/xuxiaoxiao/xiaoimageloader/util/ImageLoader.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.LruCache;
+import android.widget.ImageView;
 
 import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
@@ -20,7 +21,7 @@ public class ImageLoader {
     /**
      * 图片缓存的核心对象
      */
-    private LruCache<String,Bitmap> mLruCache;
+    private LruCache<String, Bitmap> mLruCache;
 
     /**
      * 线程池
@@ -48,33 +49,30 @@ public class ImageLoader {
      * UI线程中的Handler
      */
     private Handler mUIHandler;
-    public enum Type
-    {
-        FIFI,LIFO;
+
+    public enum Type {
+        FIFI, LIFO;
     }
 
-    private ImageLoader(int threadCount, Type type){
-        init(threadCount,type);
+    private ImageLoader(int threadCount, Type type) {
+        init(threadCount, type);
     }
 
     /**
      * 初始化操作
+     *
      * @param threadCount
      * @param type
      */
     private void init(int threadCount, Type type) {
         // 后台轮询线程
-        mPoolThread = new Thread()
-        {
+        mPoolThread = new Thread() {
             @Override
-            public void run()
-            {
+            public void run() {
                 Looper.prepare();
-                mPoolThreadHandler = new Handler()
-                {
+                mPoolThreadHandler = new Handler() {
                     @Override
-                    public void handleMessage(Message msg)
-                    {
+                    public void handleMessage(Message msg) {
                         // 去线程池取出一个任务去执行
                     }
                 };
@@ -83,9 +81,9 @@ public class ImageLoader {
         };
         mPoolThread.start();
         // 获取我们应用的最大可用内存
-        int maxMemory = (int)Runtime.getRuntime().maxMemory();
+        int maxMemory = (int) Runtime.getRuntime().maxMemory();
         int cacheMemory = maxMemory / 8;
-        mLruCache = new LruCache<String, Bitmap>(cacheMemory){
+        mLruCache = new LruCache<String, Bitmap>(cacheMemory) {
             @Override
             protected int sizeOf(String key, Bitmap value) {
                 return value.getRowBytes() * value.getHeight();
@@ -97,15 +95,68 @@ public class ImageLoader {
         mType = type;
     }
 
-    public static ImageLoader getmInstance(){
-        if (mInstance == null){
-            synchronized (ImageLoader.class)
-            {
-                if (mInstance == null){
-                    mInstance = new ImageLoader(DEFAULT_THREAD_COUNT,Type.LIFO);
+    public static ImageLoader getmInstance() {
+        if (mInstance == null) {
+            synchronized (ImageLoader.class) {
+                if (mInstance == null) {
+                    mInstance = new ImageLoader(DEFAULT_THREAD_COUNT, Type.LIFO);
                 }
             }
         }
         return mInstance;
+    }
+
+    /**
+     * 根据path为imageView设置图片
+     *
+     * @param path
+     * @param imageView
+     */
+    public void LoadImage(String path, final ImageView imageView) {
+        imageView.setTag(path);
+        if (mUIHandler == null) {
+            mUIHandler = new Handler() {
+                public void handleMessage(Message msg) {
+                    // 获取得到图片，为imageView回调设置图片
+                    ImgBeanHolder holder = (ImgBeanHolder) msg.obj;
+                    Bitmap bm = holder.bitmap;
+                    ImageView imageview = holder.imageView;
+                    String path = holder.path;
+
+                    // 将path与getTag存储路径进行比较
+                    if (imageview.getTag().toString().equals(path)) {
+                        imageview.setImageBitmap(bm);
+                    }
+                }
+            };
+        }
+        // 根据path在缓存中获取bitmap
+        Bitmap bm = getBitmapFromLruCache(path);
+        if (bm != null) {
+            Message message = Message.obtain();
+            ImgBeanHolder holder = new ImgBeanHolder();
+            holder.bitmap = bm;
+            holder.path = path;
+            holder.imageView = imageView;
+            message.obj = holder;
+            mUIHandler.sendMessage(message);
+        }
+    }
+
+
+    /**
+     * 根据path在缓存中获取bitmap
+     *
+     * @param key
+     * @return
+     */
+    private Bitmap getBitmapFromLruCache(String key) {
+        return mLruCache.get(key);
+    }
+
+    private class ImgBeanHolder {
+        Bitmap bitmap;
+        ImageView imageView;
+        String path;
     }
 }

9c81cfb0316ce16592300e1838d5c918a1e52631
xiaoqianghou <2662651103@qq.com>
2017-05-27 21:56:19 +0800

    ImageLoader变量初始化(这次是对的)

--- app/src/main/java/org/xuxiaoxiao/xiaoimageloader/util/ImageLoader.java
+++ app/src/main/java/org/xuxiaoxiao/xiaoimageloader/util/ImageLoader.java
@@ -2,10 +2,13 @@ package org.xuxiaoxiao.xiaoimageloader.util;
 
 import android.graphics.Bitmap;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.LruCache;
 
 import java.util.LinkedList;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by WuQiang on 2017/5/27.
@@ -50,19 +53,59 @@ public class ImageLoader {
         FIFI,LIFO;
     }
 
-    private ImageLoader(){
-
+    private ImageLoader(int threadCount, Type type){
+        init(threadCount,type);
     }
+
+    /**
+     * 初始化操作
+     * @param threadCount
+     * @param type
+     */
+    private void init(int threadCount, Type type) {
+        // 后台轮询线程
+        mPoolThread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                Looper.prepare();
+                mPoolThreadHandler = new Handler()
+                {
+                    @Override
+                    public void handleMessage(Message msg)
+                    {
+                        // 去线程池取出一个任务去执行
+                    }
+                };
+                Looper.loop();
+            }
+        };
+        mPoolThread.start();
+        // 获取我们应用的最大可用内存
+        int maxMemory = (int)Runtime.getRuntime().maxMemory();
+        int cacheMemory = maxMemory / 8;
+        mLruCache = new LruCache<String, Bitmap>(cacheMemory){
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getRowBytes() * value.getHeight();
+            }
+        };
+        // 创建线程池
+        mThreadPool = Executors.newFixedThreadPool(threadCount);
+        mTaskQueue = new LinkedList<Runnable>();
+        mType = type;
+    }
+
     public static ImageLoader getmInstance(){
         if (mInstance == null){
             synchronized (ImageLoader.class)
             {
                 if (mInstance == null){
-                    mInstance = new ImageLoader();
+                    mInstance = new ImageLoader(DEFAULT_THREAD_COUNT,Type.LIFO);
                 }
             }
         }
         return mInstance;
     }
-    // 第二次commit错了
 }

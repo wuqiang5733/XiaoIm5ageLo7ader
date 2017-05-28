42634ed4d78d417190a0bcfecf36360ea294c9b7
xiaoqianghou <2662651103@qq.com>
2017-05-28 20:47:42 +0800

    初始化控件

--- app/src/main/java/org/xuxiaoxiao/xiaoimageloader/MainActivity.java
+++ app/src/main/java/org/xuxiaoxiao/xiaoimageloader/MainActivity.java
@@ -1,13 +1,52 @@
 package org.xuxiaoxiao.xiaoimageloader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.xuxiaoxiao.xiaoimageloader.bean.FolderBean;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private GridView mGridView;
+    private List<String> mImgs;
+
+    private RelativeLayout mBottomLy;
+    private TextView mDirName;
+    private TextView mDirCount;
+
+    private File mCurrentDir;
+    private int mMaxCount;
+
+    private List<FolderBean> mFolderBeans = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+        initEvent();
     }
+
+    private void initView() {
+        mGridView = (GridView) findViewById(R.id.id_gridView);
+        mBottomLy = (RelativeLayout) findViewById(R.id.id_bottom_ly);
+        mDirName = (TextView) findViewById(R.id.id_dir_name);
+        mDirCount = (TextView) findViewById(R.id.id_dir_count);
+    }
+
+    private void initData() {
+    }
+
+    private void initEvent() {
+    }
+
+
 }
---
+++ app/src/main/java/org/xuxiaoxiao/xiaoimageloader/bean/FolderBean.java
@@ -0,0 +1,49 @@
+package org.xuxiaoxiao.xiaoimageloader.bean;
+
+/**
+ * Created by WuQiang on 2017/5/28.
+ */
+
+public class FolderBean {
+    /**
+     * 当前文件夹的路径
+     */
+    private String dir;
+    private String firstImgPath;
+    private String name;
+    private int count;
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+        int lastIndexOf = this.dir.indexOf("/");
+        this.name = this.dir.substring(lastIndexOf);
+    }
+
+    public String getFirstImgPath() {
+        return firstImgPath;
+    }
+
+    public void setFirstImgPath(String firstImgPath) {
+        this.firstImgPath = firstImgPath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
